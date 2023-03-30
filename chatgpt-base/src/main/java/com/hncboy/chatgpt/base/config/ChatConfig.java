c0ab207b887e5dccda41d3c59d81df4f6ac06e4e
lizhongyuan3 <lizhongyuan3@yeah.net>
2023-03-30 15:49:01 +0800

    ip 限流修改 LoadingCache 为 ConcurrentMap

--- chatgpt-base/src/main/java/com/hncboy/chatgpt/base/config/ChatConfig.java
+++ chatgpt-base/src/main/java/com/hncboy/chatgpt/base/config/ChatConfig.java
@@ -82,30 +82,12 @@ public class ChatConfig implements InitializingBean {
      * 单位 maxRequestSecond 的最大请求数
      * 默认1s1次
      */
-    private static Integer maxRequest;
+    private Integer maxRequest;
 
     /**
      * 多少s
      */
-    private static Integer maxRequestSecond;
-
-    /**
-     * 设置最大请求数
-     *
-     * @param maxRequest 最大请求数
-     */
-    public void setMaxRequest(Integer maxRequest) {
-        ChatConfig.maxRequest = maxRequest;
-    }
-
-    /**
-     * 设置单位时间
-     *
-     * @param maxRequestSecond 单位时间
-     */
-    public void setMaxRequestSecond(Integer maxRequestSecond) {
-        ChatConfig.maxRequestSecond = maxRequestSecond;
-    }
+    private Integer maxRequestSecond;
 
     /**
      * 判断是否有 http 代理
@@ -138,21 +120,11 @@ public class ChatConfig implements InitializingBean {
         return ApiTypeEnum.ACCESS_TOKEN;
     }
 
-    /**
-     * 获取 ip 限流最大请求数
-     *
-     * @return 最大请求数
-     */
-    public static Integer getMaxRequest() {
+    public Integer getMaxRequest() {
         return Opt.ofNullable(maxRequest).orElse(1);
     }
 
-    /**
-     * 获取 ip 限流单位时间
-     *
-     * @return 单位时间
-     */
-    public static Integer getMaxRequestSecond() {
+    public Integer getMaxRequestSecond() {
         return Opt.ofNullable(maxRequestSecond).orElse(1);
     }
 
--- chatgpt-base/src/main/java/com/hncboy/chatgpt/base/handler/IpRateLimiterHandler.java
+++ chatgpt-base/src/main/java/com/hncboy/chatgpt/base/handler/IpRateLimiterHandler.java
@@ -1,17 +1,13 @@
 package com.hncboy.chatgpt.base.handler;
 
-import cn.hutool.core.lang.Opt;
 import cn.hutool.core.util.NumberUtil;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import cn.hutool.extra.spring.SpringUtil;
+import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.RateLimiter;
 import com.hncboy.chatgpt.base.config.ChatConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
 
 /**
  * @author lizhongyuan
@@ -20,34 +16,21 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class IpRateLimiterHandler {
 
-    private static final LoadingCache<String, RateLimiter> IP_CACHE = CacheBuilder.newBuilder()
-            // 设置并发级别为 CPU 核心数
-            .concurrencyLevel(Runtime.getRuntime().availableProcessors())
-            .expireAfterAccess(ChatConfig.getMaxRequestSecond(), TimeUnit.SECONDS)
-            .build(new CacheLoader<>() {
-                @Override
-                public @NotNull RateLimiter load(@NotNull String s) {
-                    // 根据指定时间内的可访问数创建一个指定速率的 RateLimiter
-                    return RateLimiter.create(NumberUtil.div(ChatConfig.getMaxRequest(), ChatConfig.getMaxRequestSecond()).doubleValue());
-                }
-            });
+    private static final Map<String, RateLimiter> LIMITER_MAP = Maps.newConcurrentMap();
 
     /**
-     * 根据 ip 从缓存中获取其限流器
+     * 根据ip请求是否被限制请求
      *
      * @param ip ip
      * @return true 允许请求
      */
     public static boolean allowRequest(String ip) {
-        try {
-            // 从缓存中获取对应的RateLimiter对象
-            RateLimiter rateLimiter = IP_CACHE.get(ip);
-            // 尝试获取令牌，如果获取成功则说明可以通过请求
-            return Opt.ofNullable(rateLimiter.tryAcquire()).orElse(false);
-        } catch (ExecutionException e) {
-            log.error("限流器获取异常:[{}]", e.getMessage());
-            return false;
-        }
+        ChatConfig chatConfig = SpringUtil.getBean(ChatConfig.class);
+        // 从 Map 中获取对应的RateLimiter对象
+        return LIMITER_MAP.computeIfAbsent(
+                ip,
+                key -> RateLimiter.create(NumberUtil.div(chatConfig.getMaxRequest(), chatConfig.getMaxRequestSecond()).doubleValue())
+        ).tryAcquire();
     }
 
 }
